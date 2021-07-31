c042848c4bc2502e9fbd58a09242affc96be114f
John Malek <johnmalek54@gmail.com>
2021-07-31 14:59:43 +0300

    Add files via upload

---
+++ Exercise04.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	printf("Loading shapes from XML: ");
+	printf("\nCircle is Red and has an area of %f", 3.142 * 2.5 * 2.5);
+	printf("\nRectangle is Blue and has an area of %1f", 20.0 * 10.0);
+	printf("\nCircle is Green and has an area of %f", 3.142 * 8.0 * 8.0);
+	printf("\nCircle is Purple and has an area of %f", 3.142 * 12.3 * 12.3);
+	printf("\nRectangle is Blue and has an area of %1f",  45.0 * 18.0);
+
+	printf("\n\n");
+
+return 0;
+
+}
+
+// I do not have any experience given the question that has been asked. My knowledge in C# is purely new and needs application and further training. Thank you!
